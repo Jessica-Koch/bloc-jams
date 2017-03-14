59d2010129fd4163ef4540c0710e3166c5f386b4
Jessica-Koch <jessicakoch136@icloud.com>
2017-03-14 01:23:28 -0700

    refactor code and stylize icons

--- scripts/landing.js
+++ scripts/landing.js
@@ -28,3 +28,22 @@
      revealThirdPoint();
  
  };
+
+
+  var animatePoints = function() {
+ 
+    var points = document.getElementsByClassName('point');
+ 
+    var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(points[i]);
+     };
+ 
+ 
+ };
